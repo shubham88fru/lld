9368df21c7a3f0c0beac0959b9ff6be8bb5623e6
shubham88fru <shubham88fru@yahoo.com>
2024-03-22 19:41:16 -0700

    builder

---
+++ src/main/java/designpatterns/coffeepoweredcrew/creational/builder/Client.java
@@ -0,0 +1,39 @@
+package designpatterns.coffeepoweredcrew.creational.builder;
+
+import java.time.LocalDate;
+
+//This is our client which also works as "director"
+public class Client {
+
+	public static void main(String[] args) {
+		User user = createUser();
+		UserDTOBuilder builder = new UserWebDTOBuilder();
+		directBuild(builder, user);
+	}
+
+	private static UserDTO directBuild(UserDTOBuilder builder, User user) {
+		return builder.withFirstName(user.getLastName())
+				.withLastName(user.getLastName())
+				.withAddress(user.getAddress())
+				.withBirthday(user.getBirthday())
+				.build();
+	}
+	
+	/**
+	 * Returns a sample user. 
+	 */
+	public static User createUser() {
+		User user = new User();
+		user.setBirthday(LocalDate.of(1960, 5, 6));
+		user.setFirstName("Ron");
+		user.setLastName("Swanson");
+		Address address = new Address();
+		address.setHouseNumber("100");
+		address.setStreet("State Street");
+		address.setCity("Pawnee");
+		address.setState("Indiana");
+		address.setZipcode("47998");
+		user.setAddress(address);
+		return user;
+	}
+}
---
+++ src/main/java/designpatterns/coffeepoweredcrew/creational/builder/UserDTO.java
@@ -0,0 +1,11 @@
+package designpatterns.coffeepoweredcrew.creational.builder;
+
+//Interface implemented by "products"
+public interface UserDTO {
+	
+	String getName();
+
+	String getAddress();
+
+	String getAge();
+}
