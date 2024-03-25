6d92b24b8f7a18df5c4663123a03fa793a1ae061
shubham88fru <shubham88fru@yahoo.com>
2024-03-24 18:46:12 -0700

    simple factory pattern

---
+++ src/main/java/designpatterns/coffeepoweredcrew/creational/simplefactory/Client.java
@@ -0,0 +1,12 @@
+package designpatterns.coffeepoweredcrew.creational.simplefactory;
+
+public class Client {
+
+	public static void main(String[] args) {
+		Post blog = PostFactory.createPost("news");
+		System.out.println(blog);
+		Post news = PostFactory.createPost("news");
+		System.out.println(news);
+	}
+
+}
