2137531575f73bc5fd88d781afd7a7cea5f99a45
shubham88fru <shubham88fru@yahoo.com>
2024-05-06 17:27:20 -0700

    composite pattern

---
+++ src/main/java/designpatterns/coffeepoweredcrew/structural/composite/Directory.java
@@ -0,0 +1,34 @@
+package designpatterns.coffeepoweredcrew.structural.composite;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//Composite in the composite pattern
+public class Directory extends File {
+	private List<File> children = new ArrayList<>();
+	public Directory(String name) {
+        super(name);
+    }
+
+    @Override
+    public void ls() {
+        System.out.println(getName());
+        for (File file: children) file.ls();
+    }
+
+    @Override
+    public void addFile(File file) {
+        children.add(file);
+    }
+
+    @Override
+    public File[] getFiles() {
+        return children.toArray(new File[children.size()]);
+    }
+
+    @Override
+    public boolean removeFile(File file) {
+        return children.remove(file);
+    }
+
+}
