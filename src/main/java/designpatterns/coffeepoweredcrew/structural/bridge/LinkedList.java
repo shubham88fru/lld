b552ef0e01b924239dfca2854dca8822ca1c6401
shubham88fru <shubham88fru@yahoo.com>
2024-11-03 18:32:41 -0700

    bridge pattern

--- src/main/java/designpatterns/coffeepoweredcrew/structural/bridge/LinkedList.java
+++ src/main/java/designpatterns/coffeepoweredcrew/structural/bridge/LinkedList.java
@@ -2,7 +2,7 @@ package designpatterns.coffeepoweredcrew.structural.bridge;
 
 //This is the implementor. 
 //Note that this is also an interface
-//As implementor is defining its own hierarchy which not related
+//As implementor is defining its own hierarchy which is not related
 //at all to the abstraction hierarchy. 
 public interface LinkedList<T> {
 	
